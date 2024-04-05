46cc1bfcbd7e7f5be8885beb64b5583074a8706b
Miles544 <162893957+ngk0@users.noreply.github.com>
2024-04-05 07:54:04 -0400

    revert Max steer back to 250

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -54,7 +54,7 @@ class CarControllerParams:
     elif CP.carFingerprint in CUSW_CARS:
       self.STEER_DELTA_UP = 4
       self.STEER_DELTA_DOWN = 4
-      self.STEER_MAX = 261  # TODO: re-validate this, Panda is at 261
+      self.STEER_MAX = 250  # TODO: re-validate this, Panda is at 261, 19 Cherokee KL faults at 261.  eps or panda?
     else:
       self.STEER_DELTA_UP = 3
       self.STEER_DELTA_DOWN = 3
